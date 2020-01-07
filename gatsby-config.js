dc3feb80ebddcf4f8859d812dfa01fb623ca7fbc
Tom Bear <tom@mrbear.info>
2020-01-07 14:08:26 +0000

    added gatsby-plugin-transition-link

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,19 +16,10 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: "gatsby-plugin-transition-link",
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+        layout: require.resolve(`./src/components/layout.js`)
+      }
+    }
   ],
 }
